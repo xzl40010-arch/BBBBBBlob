50c0e5e919977dd5f958ef901210f674fa95608c
WILLmag1c <2578495977@qq.com>
2026-02-05 14:21:16 +0800

    解决画质问题
    修改相关设置和脚本，调整了摄像机设置和其他相关脚本，将大部分Pixel Perfect Camera（PPU）统一，解决像素无法整齐落齐和渲染异常问题

--- New Unity Project/Assets/Scripts/CameraFollow.cs
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -1,6 +1,9 @@
 //2026.2.2 郑佳鑫
 //第一次修改：摄像机跟随脚本
+//2.5 文振一
+//第二次修改：解决平滑跟随导致的人物角色轻微糊/抖动问题
 using UnityEngine;
+
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
@@ -16,5 +19,12 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 desiredPosition = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        //26.2.5 新增
+        int PPU = 24;
+        float unit = 1f / PPU;
+        Vector3 p = transform.position;
+        p.x = Mathf.Round(p.x / unit) * unit;
+        p.y = Mathf.Round(p.y / unit) * unit;
+        transform.position = p;
     }
 }
