f9e7bb7e6297f65fbcbb59968fb34700459153f2
WILLmag1c <2578495977@qq.com>
2026-02-05 14:41:17 +0800

    毁了，xzl王超立马
    This reverts commit 50c0e5e919977dd5f958ef901210f674fa95608c.

--- New Unity Project/Assets/Scripts/CameraFollow.cs
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -1,9 +1,6 @@
 //2026.2.2 郑佳鑫
 //第一次修改：摄像机跟随脚本
-//2.5 文振一
-//第二次修改：解决平滑跟随导致的人物角色轻微糊/抖动问题
 using UnityEngine;
-
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
@@ -19,12 +16,5 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 desiredPosition = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-        //26.2.5 新增
-        int PPU = 24;
-        float unit = 1f / PPU;
-        Vector3 p = transform.position;
-        p.x = Mathf.Round(p.x / unit) * unit;
-        p.y = Mathf.Round(p.y / unit) * unit;
-        transform.position = p;
     }
 }
